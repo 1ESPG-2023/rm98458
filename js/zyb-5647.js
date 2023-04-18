6ece4d0b5d64ea136c0429977a1ab63e134c8015
JLucazY <rm98458@fiap.com.br>
2023-04-18 11:08:09 -0300

    Estrutura inicial para o EX: funções

--- js/zyb-5647.js
+++ js/zyb-5647.js
@@ -1,19 +1,44 @@
-const liElement = document.getElementsByTagName("li");
 
-// for (let index = 0; index < liElement.length; index++) {
-//     console.log(liElement[index].textContent)    
-// }
+// // for (let index = 0; index < liElement.length; index++) {
+// //     console.log(liElement[index].textContent)    
+// // }
 
-// console.log(liElement)
+// // console.log(liElement)
 
-let nr1 = [1,2,3,4,5];
-let nr2 = [6,7,8,9,10];
-let nr3 = [nr1,nr2]
+// // let nr1 = [1,2,3,4,5];
+// // let nr2 = [6,7,8,9,10];
+// // let nr3 = [nr1,nr2]
 
-console.log(`Array-1 ${nr1}`);
-console.log(`Array-2 ${nr2}`);
-console.log(`Array-3 ${nr3}`);
+// // console.log(`Array-1 ${nr1}`);
+// // console.log(`Array-2 ${nr2}`);
+// // console.log(`Array-3 ${nr3}`);
 
-nr3.forEach((itemDoArray) =>{
-    console.log(`Array-3 sendo listado ${itemDoArray}`);
-})
+// // nr3.forEach((itemDoArray) =>{
+// //     console.log(`Array-3 sendo listado ${itemDoArray}`);
+// // })
+
+
+// // // listando array com MAP
+// // nr3.map((numero,key)=>{
+// //      log(`Itens do array - ${(key+1) - numero}`);
+// // })
+
+
+// // let nr1 = [1,2,3,4,5];
+// // let nr2 = [6,7,8,9,10];
+// // //Espalhar o array com SPREAD [ ... ]
+// // let nr3 = [...nr1,nr2]
+
+// // console.log(`Array-1 ${nr1}`);
+// // console.log(`Array-2 ${nr2}`);
+// // console.log(`Array-3 ${nr3}`);
+
+
+
+// //CONVERTENDO HTML COLLECTION EM ARRAY DE FORMA INDIRETA 
+// const liElementHtml = document.getElementsByTagName("li");
+// const liElementArray = [...liElementHtml];
+
+// liElementArray.map((item,key)=>{
+//     console.log(``)
+// })
